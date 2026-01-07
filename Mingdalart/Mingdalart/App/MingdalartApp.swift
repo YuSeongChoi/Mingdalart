1c5be7f1d8cbc5b756b482031a7fdebd3e829a7f
cys <cys@iquest.kr>
2026-01-07 16:17:38 +0900

    setup: 프로젝트 생성

---
+++ Mingdalart/Mingdalart/App/MingdalartApp.swift
@@ -0,0 +1,17 @@
+//
+//  MingdalartApp.swift
+//  Mingdalart
+//
+//  Created by YuSeongChoi on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct MingdalartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
